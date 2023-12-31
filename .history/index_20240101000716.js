e21a5e0e7a40a0f754e6b23f5f1b9c6857ca0109
nghiakma <lam47788@gmail.com>
2024-01-01 00:07:25 +0700

    first commit

--- .history/index_20240101000716.js
+++ .history/index_20240101000716.js
@@ -22,11 +22,7 @@ app.use(cors(
     }
 ));
 app.use(express.json())
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-  });
+
 require('./routers/history.router')(app);
 require('./routers/user.router')(app);
 require('./routers/admin.router')(app);
